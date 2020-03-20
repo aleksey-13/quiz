e9f0663137e0a7f554d65820a0dd15d7fe5d36a2
aleksey-13 <alekseymagadan@gmail.com>
2020-03-20 18:26:50 +0200

    add gfirebase stogare

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Loader.css";
+
+const Loader = () => (
+  <div className="loadingio-spinner-ripple-264wei286f8">
+    <div className="ldio-uamrdrbavas">
+      <div />
+      <div />
+    </div>
+  </div>
+);
+
+export default Loader;
